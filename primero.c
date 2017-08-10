a6496b63e6dbfd080bec42db209b244e3621dbf7
JuanMosquera <jm1128un@gmail.com>
2017-08-09 22:29:00 -0500

    el del profe

--- primero.c
+++ primero.c
@@ -1,51 +1,51 @@
-
 #include <stdio.h>
-#include <stdlib.h>
 #include <time.h>
-#include <sis/time.h>
-
+#include <sys/time.h>
+#include <stdlib.h>
 
 #define MAX 1000
 
-int main(void)
-{
-	double A[MAX][MAX], x[MAX], y[MAX];
-	double r, elapsedTime;
-	int i,j;
-	struct t2,t1;
-	{
-		
-	};
+int main(void){
+  double A[MAX][MAX], x[MAX], y[MAX], elapsedTime, r;
+  int i, j;
 
-	//initialization
-	srand(time(NULL));
+  struct timeval t1, t2;
+  
+  // initialization
+  srand(time(NULL));
 
-	for(i=0;i<MAX;i++){
-		for(j=0;j<MAX;j++){
-			r=rand();
-			A[i][j]=r;
-		}
-		r=rand();
-		x[i]=r;
-		y[i]=0;
-	}
-	printf("intialized\n");
+  for(i = 0; i < MAX; i++){
+    for(j = 0; j < MAX; j++){
+      r = rand();
+      A[i][j] = r;
+    }
+    r = rand();
+    x[i] = r;
+    y[i] = 0;
+  }  
+  printf("Initialized \n");
+  // Loops
+  // First case
+  gettimeofday(&t1, NULL);
+  for(i = 0; i < MAX; i++){
+    for(j = 0; j < MAX; j++){
+      y[i] += A[i][j]*x[j];
+    }
+  }
+  gettimeofday(&t2, NULL);
+  elapsedTime = (t2.tv_sec - t1.tv_sec) * 1000.0;      // sec to ms
+  elapsedTime += (t2.tv_usec - t1.tv_usec) / 1000.0;   // us to ms
+  printf("First case %f\n", elapsedTime);
 
-	gettimeofday(&t1,NULL);
-	for (int i = 0; i < MAX; ++i)
-	{
-		for (int j = 0; j < MAX; j++)
-		{
-			y[i]+= A[i][j]*x[j];
-		}
-		
-	}
-	gettimeofday(&t2,NULL);
-	elapsedTime = (t2.tv_sec -t1.tv_sec)*1000.0;//seg a ms
-	elapsedTime += (t2.tv_usec -t1.tv_usec)/1000.0;//us a ms
-
-	printf("first case %f\n",elapsedTime);
-
-	return 0;
+  // Second case
+  gettimeofday(&t1, NULL);
+  for(j = 0; j < MAX; j++){
+    for(i = 0; i < MAX; i++){
+      y[i] += A[i][j]*x[j];
+    }
+  }
+  gettimeofday(&t2, NULL);
+  elapsedTime = (t2.tv_sec - t1.tv_sec) * 1000.0;      // sec to ms
+  elapsedTime += (t2.tv_usec - t1.tv_usec) / 1000.0;   // us to ms
+  printf("Second case %f\n", elapsedTime);
 }
-	
